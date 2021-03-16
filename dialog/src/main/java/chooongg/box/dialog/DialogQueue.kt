d439be0d70b70e05fd29f6fa47d40dd80a14f669
chooongg <chooongg@outlook.com>
2021-03-16 17:34:55 +0800

    首次提交

---
+++ dialog/src/main/java/chooongg/box/dialog/DialogQueue.kt
@@ -0,0 +1,16 @@
+package chooongg.box.dialog
+
+import android.app.Dialog
+import java.util.*
+
+object DialogQueue {
+
+    private val dialogs = LinkedList<Dialog>()
+
+    fun show(dialog: Dialog) {
+    }
+
+    fun cancelAll(){
+
+    }
+}
