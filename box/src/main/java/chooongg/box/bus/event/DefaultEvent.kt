9aabe3bc50d3fab82ff542dd283610bae5be3442
Chooongg <chooongg@outlook.com>
2021-06-10 15:09:35 +0800

    更新版本0.0.7 添加LiveDataBus

---
+++ box/src/main/java/chooongg/box/bus/event/DefaultEvent.kt
@@ -0,0 +1,57 @@
+package chooongg.box.bus.event
+
+import androidx.activity.ComponentActivity
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.*
+
+/**
+ * 普通事件
+ */
+class DefaultEvent(
+    private val liveDataMap: HashMap<String, MutableLiveData<Any?>>,
+    private val tag: String
+) : Event {
+
+
+    override fun observe(activity: ComponentActivity, observer: Observer<Any?>) {
+        subscribe(activity, observer)
+    }
+
+    override fun observe(fragment: Fragment, observer: Observer<Any?>) {
+        subscribe(fragment, observer)
+    }
+
+    override fun observeForever(activity: ComponentActivity, observer: Observer<Any?>) {
+        subscribeForever(activity, observer)
+    }
+
+    override fun observeForever(fragment: Fragment, observer: Observer<Any?>) {
+        subscribeForever(fragment, observer)
+    }
+
+    private fun subscribe(owner: LifecycleOwner, observer: Observer<Any?>) {
+        liveDataMap[tag] = MutableLiveData()
+        liveDataMap[tag]?.observe(owner, { o ->
+            observer.onChanged(o)
+        })
+    }
+
+
+    @Suppress("unused")
+    private fun subscribeForever(owner: LifecycleOwner, observer: Observer<Any?>) {
+        liveDataMap[tag] = MutableLiveData()
+        val mObserver = Observer<Any?> { o ->
+            observer.onChanged(o)
+        }
+
+        liveDataMap[tag]?.observeForever(mObserver)
+
+        //onDestroy时解绑
+        owner.lifecycle.addObserver(object : LifecycleObserver {
+            @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+            fun onDestroy() {
+                liveDataMap[tag]?.removeObserver(mObserver)
+            }
+        })
+    }
+}
