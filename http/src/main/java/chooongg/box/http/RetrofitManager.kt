8140320ada0a51022a95fc44e73b64464f9943f9
Chooongg <chooongg@outlook.com>
2021-06-16 17:01:36 +0800

    更新版本0.0.17

--- http/src/main/java/chooongg/box/http/RetrofitManager.kt
+++ http/src/main/java/chooongg/box/http/RetrofitManager.kt
@@ -8,6 +8,7 @@ import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import java.util.concurrent.TimeUnit
 import kotlin.reflect.KClass
+import kotlin.reflect.full.findAnnotation
 
 object RetrofitManager {
 
@@ -17,7 +18,17 @@ object RetrofitManager {
         this.defaultConfig = defaultConfig
     }
 
-    fun <T : Any> getAPI(clazz: KClass<T>,baseUrl:String, config: HttpConfig = defaultConfig): T {
+    fun <T : Any> getAPI(clazz: KClass<T>, config: HttpConfig = defaultConfig): T {
+        val builder = Retrofit.Builder()
+            .baseUrl(getBaseUrlForAnnotation(clazz))
+            .client(okHttpClientBuilder(config).build())
+        config.converterFactories.forEach { builder.addConverterFactory(it) }
+        config.callAdapterFactory.forEach { builder.addCallAdapterFactory(it) }
+        val retrofit = builder.build()
+        return retrofit.create(clazz.java)
+    }
+
+    fun <T : Any> getAPI(clazz: KClass<T>, baseUrl: String, config: HttpConfig = defaultConfig): T {
         val builder = Retrofit.Builder()
             .baseUrl(baseUrl)
             .client(okHttpClientBuilder(config).build())
@@ -27,21 +38,22 @@ object RetrofitManager {
         return retrofit.create(clazz.java)
     }
 
-    private fun okHttpClientBuilder(config: HttpConfig = defaultConfig) = OkHttpClient.Builder().apply {
-        connectTimeout(config.connectTimeout, TimeUnit.SECONDS)
-        writeTimeout(config.writeTimeout, TimeUnit.SECONDS)
-        readTimeout(config.readTimeout, TimeUnit.SECONDS)
-        retryOnConnectionFailure(true)
-        cache(Cache(APP.cacheDir, config.cacheSize))
-        cookieJar(CookieManager(APP))
-        config.interceptor.forEach { addInterceptor(it) }
-        addInterceptor(BoxLogInterceptor)
-        config.networkInterceptor.forEach { addNetworkInterceptor(it) }
-    }
+    private fun okHttpClientBuilder(config: HttpConfig = defaultConfig) =
+        OkHttpClient.Builder().apply {
+            connectTimeout(config.connectTimeout, TimeUnit.SECONDS)
+            writeTimeout(config.writeTimeout, TimeUnit.SECONDS)
+            readTimeout(config.readTimeout, TimeUnit.SECONDS)
+            retryOnConnectionFailure(true)
+            cache(Cache(APP.cacheDir, config.cacheSize))
+            cookieJar(CookieManager(APP))
+            config.interceptor.forEach { addInterceptor(it) }
+            addInterceptor(BoxLogInterceptor)
+            config.networkInterceptor.forEach { addNetworkInterceptor(it) }
+        }
 
-    private fun <T> getBaseUrlForAnnotation(clazz: Class<T>): String {
-        if (clazz.javaClass.isAnnotationPresent(BaseUrl::class.java)) {
-            return clazz.javaClass.getAnnotation(BaseUrl::class.java).value
-        } else throw RuntimeException("unable to find BaseUrl from Annotation")
+    private fun <T : Any> getBaseUrlForAnnotation(clazz: KClass<T>): String {
+        val findAnnotation = clazz.findAnnotation<BaseUrl>()
+            ?: throw RuntimeException("unable to find BaseUrl from Annotation")
+        return findAnnotation.value
     }
 }
